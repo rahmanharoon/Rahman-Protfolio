0aa588d7b89affba244a3275d10a1cb776d85cc8
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-01 23:28:02 +0530

    Navbar home button css added

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Link as LinkR } from 'react-router-dom'
-// import { Link as LinkS } from 'react-scroll'
+import { Link as LinkS } from 'react-scroll'
 
 export const Nav = styled.nav`
     background: #000;
@@ -38,4 +38,48 @@ export const NavLogo = styled(LinkR)`
     margin-left: 24px;
     font-weight: bold;
     text-decoration: none;
+`;
+
+export const MobileIcon = styled.div`
+    display: none;
+
+    @meda screen and (max-width: 768px) {
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-100%, 60%);
+        font-size; 1.8rem;
+        cursor: pointer;
+    }
+`;
+
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+    margin-right: -22px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+export const NavItem = styled.li`
+    height: 80px;
+`;
+
+export const NavLinks = styled(LinkS)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decorations: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+
+    &.active {
+        border-bottom: 3px solid #01bf71;
+    }
 `;
