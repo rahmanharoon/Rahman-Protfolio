c7f42134c6e33129c66082442c8fd9749e989b68
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-02 23:28:22 +0530

    Sidebar imported

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import{
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+} from './SidebarElements'
 
 const Sidebar = () => {
     return (
@@ -6,6 +11,27 @@ const Sidebar = () => {
             <Icon>
                 <CloseIcon />
             </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="discover">
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to="services">
+                        Services
+                    </SidebarLink>
+                    <SidebarLink to="contact">
+                        Connect Me
+                    </SidebarLink>
+                </SidebarMenu>
+                <SidebtnWrap>
+                    <SidebarRoute to="/hire">
+                        Hire
+                    </SidebarRoute>
+                </SidebtnWrap>
+            </SidebarWrapper>
         </SidebarContainer>
     );
 };
