3c2307eadf26be948532edcb2e417b971912a1d3
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-03 21:09:21 +0530

    sidebarElements css added through slack

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
-import {FaTimes} from 'react-icons/fa'
+import { Link as LinkS } from 'react-scroll'
+import { Link as LinkR } from 'react-router-dom'
+import { FaTimes } from 'react-icons/fa'
 
 export const SidebarContainer = styled.aside`
     position: fixed;
@@ -46,5 +48,31 @@ export const SidebarLink = styled(LinkS)`
 
     &:hover {
         color: 01bf71;
+        transition: 0.2s ease-in-out;
+    }
+`
+
+export const SideBtnWrap = styled.div`
+ display: flex;
+ justify-content: center;
+`
+
+export const SidebarRoute = styled(LinkR)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 16px 64px;
+    color: #01606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #fff;
+        color: #010606;
     }
 `
