0754b59c5097f6a092f42bfeebd678b35c2a70d8
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-11 22:01:33 +0530

    toggle action committed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -18,21 +18,21 @@ const Sidebar = ({isOpen,toggle}) => {
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to="about">
+                    <SidebarLink to="about" isOpen = {isOpen} onClick = {toggle} >
                         About
                     </SidebarLink>
-                    <SidebarLink to="discover">
+                    <SidebarLink to="discover" isOpen = {isOpen} onClick = {toggle} >
                         Discover
                     </SidebarLink>
-                    <SidebarLink to="services">
+                    <SidebarLink to="services" isOpen = {isOpen} onClick = {toggle} >
                         Services
                     </SidebarLink>
-                    <SidebarLink to="contact">
+                    <SidebarLink to="contact" isOpen = {isOpen} onClick = {toggle} >
                         Connect Me
                     </SidebarLink>
                 </SidebarMenu>
                 <SidebtnWrap>
-                    <SidebarRoute to="/hire">
+                    <SidebarRoute to="/hire" isOpen = {isOpen} onClick = {toggle} >
                         Hire
                     </SidebarRoute>
                 </SidebtnWrap>
