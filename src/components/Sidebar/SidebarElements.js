5c61a2b177a4d52fa557d9a2446e718a9e695325
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-11 22:13:51 +0530

    css isuues fixed

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -8,7 +8,7 @@ export const SidebarContainer = styled.aside`
     z-index: 999;
     width: 100%;
     height: 100%;
-    background: #0d0dod;
+    background: #0d0d0d;
     display: grid;
     align-items: center;
     top: 0;
