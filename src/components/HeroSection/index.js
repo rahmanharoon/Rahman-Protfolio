98f8b054dd56b16adef0ee8e3da371220f18ba7e
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-14 21:03:11 +0530

    vedio added

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Vedio from "../../vedios/home_vedio.mp4";
+import { HeroContainer, HeroBg, VedioBg } from './HeroElements';
 
 const HeroSection = () => {
     return (
         <HeroContainer>
             <HeroBg> 
-                <VedioBg autoPlay loop muted src={vedio} type = 'vedio/mp4' />
+                <VedioBg autoPlay loop muted src={Vedio} type = 'vedio/mp4' />
             </HeroBg>    
         </HeroContainer>
 
