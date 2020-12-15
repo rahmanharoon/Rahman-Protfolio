ce2091228c5a42e835425e3e0b6010b84757350c
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-15 23:18:59 +0530

    changed VedioBg into VideoBg

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -22,7 +22,7 @@ export const HeroBg = styled.div`
     overflow: hidden;
 `
 
-export const VedioBg = styled.video`
+export const VideoBg = styled.video`
     width: 100%;
     height: 100%;
     -o-object-fit: cover;
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Video from "../../vedios/home_vedio.mp4";
-import { HeroContainer, HeroBg, VedioBg } from './HeroElements';
+import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
 
 const HeroSection = () => {
     return (
         <HeroContainer>
             <HeroBg> 
-                <VedioBg autoPlay loop muted src={Video} type = 'video/mp4' />
+                <VideoBg autoPlay loop muted src={Video} type = 'video/mp4' />
             </HeroBg>    
         </HeroContainer>
     )
