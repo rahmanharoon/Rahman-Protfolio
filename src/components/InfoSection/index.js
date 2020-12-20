8596e3bf6479c140e90052e462e17de806816148
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-20 21:34:05 +0530

    info section changes

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -25,7 +25,10 @@ const InfoSection = ({lightBg,
                     description, 
                     buttonLabel, 
                     img, 
-                    alt
+                    alt,
+                    primary,
+                    dark,
+                    dark2
                 }) => {
     return (
         <>
@@ -38,7 +41,19 @@ const InfoSection = ({lightBg,
                                 <Heading lightText={lightText}>{headline}</Heading>
                                 <Subtitle darkText={darkText}>{description}</Subtitle>
                                 <BtnWrap>
-                                    <Button to="home">{buttonLabel}</Button>
+                                    <Button 
+                                    to='home'
+                                    smooth={true}
+                                    duration={500}
+                                    spy={true}
+                                    exact='true'
+                                    offset={-80}
+                                    primary={primary ? 1 : 0}
+                                    dark={dark ? 1: 0}
+                                    dark2={dark2 ? 1 : 0}
+                                    >
+                                    {buttonLabel}
+                                    </Button>
                                 </BtnWrap>
                             </TextWrapper>
                         </Column1>
