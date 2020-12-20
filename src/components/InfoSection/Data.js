b8d0b42490964bc5657038fe3aa270d08e7de4b9
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-20 21:08:55 +0530

    added data on about section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'RAHMAN HAROON',
+    headline: 'Self Learned Web Developer',
+    description: 'blah blah blaaah....fill that laterrrrrrrrrr',
+    buttonLabel: 'Know more',
+    imgStart: false,
+    img: require('../../images/about-image.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
