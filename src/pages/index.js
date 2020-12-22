4910ecb14af6b32644fa1f8fa66fa9bf167d36cb
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-22 23:37:40 +0530

    removed homeobjThree

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
-import { homeObjOne, homeObjThree, homeObjTwo } from '../components/InfoSection/Data';
+import { homeObjOne, homeObjTwo } from '../components/InfoSection/Data';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
@@ -17,7 +17,6 @@ const Home = () => {
             <HeroSection />
             <InfoSection {...homeObjOne} />
             <InfoSection {...homeObjTwo} />
-            <InfoSection {...homeObjThree} />
         </>
     );
 };
