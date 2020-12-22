151d8b9799b175a189db66f0d1280ecfaf120c7b
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-22 23:45:08 +0530

    imported services

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import { homeObjOne, homeObjTwo } from '../components/InfoSection/Data';
 import Navbar from '../components/Navbar';
+import Services from '../components/Services';
 import Sidebar from '../components/Sidebar';
 
 const Home = () => {
@@ -17,6 +18,7 @@ const Home = () => {
             <HeroSection />
             <InfoSection {...homeObjOne} />
             <InfoSection {...homeObjTwo} />
+            <Services />
         </>
     );
 };
