25b09fb95746e1c938e32e54c929e75cc02ba611
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-22 23:37:27 +0530

    discover page added

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -14,3 +14,19 @@ export const homeObjOne = {
     primary: true,
     darkText: false
 }
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Say somethiiggggg',
+    headline: 'fill itttt',
+    description: 'blah blah blaaah....fill that laterrrrrrrrrr',
+    buttonLabel: 'Learn more',
+    imgStart: true,
+    img: require("../../images/rh1.jpeg").default,
+    alt: 'error404',
+    dark: false,
+    primary: false,
+    darkText: true
+}
