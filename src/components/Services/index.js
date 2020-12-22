383c16ac28319ab6c6259207f09189dd3068bca4
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-23 00:11:33 +0530

    imported services elements

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Icon1 from '../../images/svg-1.svg';
+import Icon2 from '../../images/svg-2.svg';
+import Icon3 from '../../images/svg-3.svg';
+import Icon4 from '../../images/svg-4.svg';
+import {
+    ServicesContainer,
+    ServicesH1,
+    ServicesWrapper,
+    ServicesCard,
+    ServicesIcon,
+    ServicesH2,
+    ServicesP
+} from './ServicesElements';
 
 const Services = () => {
     return (
         <>
-            <Services id="services">
+            <ServicesContainer id="services">
                 <ServicesH1>Services</ServicesH1>
                 <ServicesWrapper>
                     <ServicesCard>
@@ -21,8 +34,13 @@ const Services = () => {
                         <ServicesH2>Cloud services</ServicesH2>
                         <ServicesP>Fill Itt AND mAke itt, make it betterrrr bruhhhhhh</ServicesP>
                     </ServicesCard>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon4} />
+                        <ServicesH2>Mobile app</ServicesH2>
+                        <ServicesP>Fill Itt AND mAke itt, make it betterrrr bruhhhhhh</ServicesP>
+                    </ServicesCard>
                 </ServicesWrapper>
-            </Services>
+            </ServicesContainer>
         </>
     )
 }
