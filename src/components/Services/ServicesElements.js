cb6e65ff8179434a4e1440953f7c18992dc3036f
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-24 21:41:22 +0530

    changes on styled components

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -18,12 +18,12 @@ export const ServicesContainer = styled.div`
 `;
 
 export const ServicesWrapper = styled.div`
-    max-width: 1000px;
+    max-width: 1100px;
     margin: 0 auto;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     align-items: center;
-    grid-gap: 16px;
+    grid-gap: 25px;
     padding: 0 50px;
 
     @media screen and (max-width: 1100px) {
@@ -39,12 +39,13 @@ export const ServicesWrapper = styled.div`
 export const ServicesCard = styled.div`
     background: #fff;
     display: flex;
-    flex-direction: flex-start;
+    flex-direction: column;
+    justify-content: flex-start;
     align-items: center;
     border-radius: 10px;
     max-height: 340px;
     padding: 30px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
     transition: all 0.2s ease-in-out;
 
     &:hover {
