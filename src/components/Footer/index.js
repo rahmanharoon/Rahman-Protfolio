37ce1acf4f27d28ac0aebb70e3c3a675445d4881
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-24 21:58:25 +0530

    Footer ADDED

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Me</FooterLinkTitle>
+                                <FooterLink to='/signin'>About Us</FooterLink>
+                                <FooterLink to='/signin'>Router</FooterLink>
+                                <FooterLink to='/signin'>New Route Us</FooterLink>
+                                <FooterLink to='/signin'>Lets Us</FooterLink>
+                                <FooterLink to='/signin'>Terms of services</FooterLink>
+                        </FooterLinkItems>
+                    </FooterWrapper>
+                </FooterLinksContainer>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
