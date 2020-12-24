274c823182a303b0552d66ed4642b48217c3beee
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-24 22:00:47 +0530

    multipe footer items added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,6 +15,16 @@ const Footer = () => {
                                 <FooterLink to='/signin'>Terms of services</FooterLink>
                         </FooterLinkItems>
                     </FooterWrapper>
+                    <FooterWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Me</FooterLinkTitle>
+                                <FooterLink to='/signin'>About Us</FooterLink>
+                                <FooterLink to='/signin'>Router</FooterLink>
+                                <FooterLink to='/signin'>New Route Us</FooterLink>
+                                <FooterLink to='/signin'>Lets Us</FooterLink>
+                                <FooterLink to='/signin'>Terms of services</FooterLink>
+                        </FooterLinkItems>
+                    </FooterWrapper>
                 </FooterLinksContainer>
             </FooterWrap>
         </FooterContainer>
