bd6e178fcda9254aeffb51ae1162d45dc4c8ff4a
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-27 16:13:09 +0530

    router changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,11 +8,12 @@ const Footer = () => {
                     <FooterWrapper>
                         <FooterLinkItems>
                             <FooterLinkTitle>About Me</FooterLinkTitle>
-                                <FooterLink to='/signin'>About Us</FooterLink>
-                                <FooterLink to='/signin'>Router</FooterLink>
-                                <FooterLink to='/signin'>New Route Us</FooterLink>
-                                <FooterLink to='/signin'>Lets Us</FooterLink>
-                                <FooterLink to='/signin'>Terms of services</FooterLink>
+                                <FooterLink to='/'>Home</FooterLink>
+                                <FooterLink to='/about'>About</FooterLink>
+                                <FooterLink to='/discover'>Discover</FooterLink>
+                                <FooterLink to='/services'>Services</FooterLink>
+                                <FooterLink to='/gallery'>Gallery</FooterLink>
+                                <FooterLink to='/contact'>Connect with me</FooterLink>
                         </FooterLinkItems>
                     </FooterWrapper>
                     <FooterWrapper>
