2dead72cc767b924e2680b1e68c2cf0e51ae650b
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-27 20:40:15 +0530

    imported signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Contact from '../components/Contact';
 
 const SigninPage = () => {
     return (
-        <div>
-            <h1>Sign in page</h1>
-        </div>
+        <>
+            <Contact />
+        </>
     )
 }
 
