6f939dee7cc73d614d456d574592993ad8396215
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-27 16:14:05 +0530

    router changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,7 +17,7 @@ const Navbar = ({toggle}) => {
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo to='/'>Rahman Haroon</NavLogo>
+                    <NavLogo to="/">Rahman Haroon</NavLogo>
                     <MobileIcon onClick = {toggle} >
                         <FaBars />
                     </MobileIcon>
