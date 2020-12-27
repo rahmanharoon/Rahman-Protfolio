0e883cfefcc2f020c13a04b7c8c437a2246efe65
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-27 16:19:36 +0530

    imported footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { homeObjOne, homeObjTwo } from '../components/InfoSection/Data';
 import Navbar from '../components/Navbar';
 import Services from '../components/Services';
 import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -19,6 +20,7 @@ const Home = () => {
             <InfoSection {...homeObjOne} />
             <InfoSection {...homeObjTwo} />
             <Services />
+            <Footer />
         </>
     );
 };
