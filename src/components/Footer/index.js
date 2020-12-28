991ea3009c0995c9051c796106c9f8473841d256
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-28 22:48:04 +0530

    footer links changed to a tag

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import {animateScroll as scroll} from 'react-scroll'; 
-import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { 
+    FaFacebook, 
+    FaGithub, 
+    FaInstagram, 
+    FaLinkedin, 
+    FaStackOverflow, 
+    FaTwitter, 
+    FaYoutube 
+} from 'react-icons/fa';
 import {
     FooterContainer,
     FooterWrap,
@@ -8,6 +16,7 @@ import {
     FooterLinksContainer,
     FooterLinkItems,
     FooterLink,
+    FooterSocialLink,
     FooterLinkTitle,
     SocialMedia,
     SocialMediaWrap,
@@ -34,21 +43,48 @@ const Footer = () => {
                                 <FooterLink to='discover'>Discover</FooterLink>
                                 <FooterLink to='services'>Services</FooterLink>
                                 <FooterLink to='gallery'>Gallery</FooterLink>
-                                <FooterLink to='contact'>Connect with me</FooterLink>
+                                <FooterLink to='contact'>Connect With Me</FooterLink>
+                                <FooterLink to='contact'>Terms And Condition</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
                             <FooterLinkTitle>Touch With Me</FooterLinkTitle>
-                                <FooterLink href="//www.facebook.com/rahman.haroon.7/"
-                                            target="_blank"
-                                            aria-label="Facebook">
-                                                Facebook
-                                            </FooterLink>
-                                <FooterLink to='/signin'>Router</FooterLink>
-                                <FooterLink to='/signin'>New Route Us</FooterLink>
-                                <FooterLink to='/signin'>Lets Us</FooterLink>
-                                <FooterLink to='/signin'>Terms of services</FooterLink>
+                                <FooterSocialLink 
+                                href="//www.facebook.com/rahman.haroon.7/"
+                                target="_blank"
+                                >Facebook
+                                </FooterSocialLink>
+                                <FooterSocialLink 
+                                href="//www.instagram.com/rahm.n_/?hl=en" 
+                                target="_blank" 
+                                >Instagram
+                                </FooterSocialLink>
+                                <FooterSocialLink 
+                                href="//www.linkedin.com/in/rahman-haroon-589520191/" 
+                                target="_blank" 
+                                >Linkedin
+                                </FooterSocialLink>
+                                <FooterSocialLink 
+                                href="//twitter.com/rahman__haroon" 
+                                target="_blank" 
+                                >Twitter
+                                </FooterSocialLink>
+                                <FooterSocialLink 
+                                href="//github.com/rahmanharoon" 
+                                target="_blank" 
+                                >Github
+                                </FooterSocialLink>
+                                <FooterSocialLink 
+                                href="//www.youtube.com/channel/UCXpy9U8Tm93BosZKkm_XE6A" 
+                                target="_blank" 
+                                >Youtube
+                                </FooterSocialLink>
+                                <FooterSocialLink 
+                                href="//stackoverflow.com/users/13913053/rahman-haroon?tab=profile" 
+                                target="_blank" 
+                                >Stackoverflow
+                                </FooterSocialLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
@@ -100,6 +136,13 @@ const Footer = () => {
                             aria-label="Youtube"
                             >
                             <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink 
+                            href="//stackoverflow.com/users/13913053/rahman-haroon?tab=profile" 
+                            target="_blank" 
+                            aria-label="Stackoverflow"
+                            >
+                            <FaStackOverflow />
                             </SocialIconLink>
                         </SocialIcons>
                     </SocialMediaWrap>
