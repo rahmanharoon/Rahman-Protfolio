451a859144167dbcfc40a92a591f5a9ef0ce51de
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-28 23:54:54 +0530

    servcecard heght increased

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -43,7 +43,7 @@ export const ServicesCard = styled.div`
     justify-content: flex-start;
     align-items: center;
     border-radius: 10px;
-    max-height: 340px;
+    max-height: 400px;
     padding: 30px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
     transition: all 0.2s ease-in-out;
