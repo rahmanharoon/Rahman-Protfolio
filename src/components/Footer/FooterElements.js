2a16eedb469d6be3eab1cbbd39c405a91b63796c
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-28 22:49:09 +0530

    footerSocialLink added with atag

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -66,6 +66,18 @@ export const FooterLink = styled(Link)`
     }
 `;
 
+export const FooterSocialLink = styled.a`
+    color: #fff;
+    text-decoration: none;
+    margin-bottom: 0.5rem;
+    font-size: 14px;
+
+    $:hover {
+        color: #01bf71;
+        transition: 0.3s ease-out;
+    }
+`;
+
 export const SocialMedia = styled.section`
     max-width: 1000px;
     width: 100%;
