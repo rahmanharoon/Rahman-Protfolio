a86776facf0fde365072e52637fff08123f0c228
Rahman Haroon <rahmanharoon128@gmail.com>
2020-12-30 23:20:44 +0530

    margin left added to keep image left

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -93,5 +93,6 @@ export const ImgWrap = styled.div`
 export const Img = styled.img`
     width: 100%;
     margin: 0 0 10px 0;
-    padding-right: 0;
+    margin-left: 80px;
+    padding-right: 0px;
 `;
