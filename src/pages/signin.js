c8fe791a82a41faa802fc6956187b3efe4f841cb
Rahman Haroon <rahmanharoon128@gmail.com>
2021-01-05 23:49:25 +0530

    navbar imported

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Contact from '../components/Contact';
+import Navbar from '../components/Navbar';
 
 const SigninPage = () => {
     return (
         <>
+            <Navbar />
             <Contact />
         </>
     )
