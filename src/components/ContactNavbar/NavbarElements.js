f5d5708b93dea039232cc3a7bfc52a1591d5067c
Rahman Haroon <rahmanharoon128@gmail.com>
2021-01-06 00:16:27 +0530

    removed navbtn and navbtn link

--- src/components/ContactNavbar/NavbarElements.js
+++ src/components/ContactNavbar/NavbarElements.js
@@ -94,32 +94,3 @@ export const NavLinks = styled(LinkS)`
         transition: 0.3s ease-out;
     }
 `;
-
-export const NavBtn = styled.nav`
-    display: flex;
-    align-items: center;
-
-    @media screen and (max-width: 768px){
-        display: none;
-    }
-`
-
-export const NavBtnLink = styled(LinkR)`
-    border-radius: 50px;
-    background: #01bf71;
-    white-space: nowrap;
-    padding: 10px 22px;
-    color: #010606;
-    font-size: 16px;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    text-decoration: none;
-
-    &:hover {
-        transition: all 0.2s ease-in-out;
-        background: #fff;
-        color: #010606;
-    }
-`
