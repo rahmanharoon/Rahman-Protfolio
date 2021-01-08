0e20bfde32927268d3ad4664d5d0cdf230771d0e
Rahman Haroon <rahmanharoon128@gmail.com>
2021-01-08 23:07:13 +0530

    imported section on contact page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -2,6 +2,12 @@ import React, {useState} from 'react';
 import Contact from '../components/Contact';
 import ContactNavbar from '../components/ContactNavbar';
 import ContactSidebar from '../components/ContactSidebar';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne } from '../components/InfoSection/Data';
+import Services from '../components/Services';
+import Footer from '../components/Footer';
+import DiscoverSection from '../components/DiscoverSection';
+import { homeObjTwo } from '../components/DiscoverSection/Data';
 
 const SigninPage = () => {
 
@@ -15,6 +21,10 @@ const SigninPage = () => {
             <ContactSidebar isOpen = {isOpen} toggle = {toggle} />
             <ContactNavbar toggle = {toggle} />
             <Contact />
+            <InfoSection {...homeObjOne} />
+            <DiscoverSection {...homeObjTwo} />
+            <Services />
+            <Footer />
         </>
     )
 }
